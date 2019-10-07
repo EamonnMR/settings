b90e96656f4f52e14792c49f6532742f61a6d21f
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2019-10-06 22:40:59 -0400

    Fresh install script
    I'm planning on upgrading to a new OS soon, and want to have as many tools ready to go as possible when I get it running.

---
+++ setup_ubuntu.sh
@@ -0,0 +1,64 @@
+# Latest Packages
+sudo apt update
+
+sudo apt upgrade
+
+# Basic Development Tools
+sudo apt install
+  vim \
+  gimp \
+  blender \
+  git \
+  xclip \
+  ack-grep \
+  tmux \
+  curl \
+  wget \
+  
+# Games
+sudo apt install \
+  mupen64 \
+  dosbox \
+  wine \
+  gnash \
+  browser-plugin-freshplayer-pepperflash \
+  
+# Media
+sudo apt install \
+  vlc \
+  unar \
+  audacity \
+
+# Misc Tools
+sudo apt install \
+  build-essential \
+  tiled \
+  audacity \
+  codeblocks \
+  guile-2.0-dev \
+  jsonlint \
+  docker.io \
+  libglu1-mesa \
+  sbcl \
+  htop \
+  libsdl2-dev \
+  
+sudo snap install go --classic
+
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
+
+curl https://sh.rustup.rs -sSf | sh
+
+echo "https://dzone.com/articles/how-turn-firefox-browser-cache"
+
+echo "https://flutter.dev/docs/get-started/install/linux"
+
+curl -f https://github.com/BabylonJS/BlenderExporter/raw/master/Blender2Babylon-6.2.zip
+
+echo "Install BabylonJS Blender Plugin:"
+
+echo "https://doc.babylonjs.com/resources/blender"
+
+echo "Download Renoise"
