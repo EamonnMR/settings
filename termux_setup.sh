4e4eb9b47deaf1198111420e6d06221da56d3572
Eamonn McHugh-Roohr <eamonn.m.r@gmail.com>
2019-10-09 08:39:28 -0400

    Wait, termux does not ship with vim?
    heresy

--- termux_setup.sh
+++ termux_setup.sh
@@ -1,3 +1,10 @@
-PKG install git
+pkg install git
 pip install virtualenv
 pkg install dart
+pkg install vim
+
+git clone git@github.com:EamonnMR/settings.git
+
+cp settings/.vimrc .
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
